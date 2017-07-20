69663543f3fc4d98592a8429c50b5082da358c63
DanielMoisa <devmoisa@gmail.com>
2017-07-20 20:06:11 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,11 +24,7 @@ else{
 }
 });
 
-jQuery(document).ready(function($) {  
-
-// site preloader -- also uncomment the div in the header and the css style for #preloader
-$(window).load(function(){
-  $('#preloader').fadeOut('slow',function(){$(this).remove();});
-});
-
+//Preloader
+$(window).load(function() {
+  $("body").removeClass("preload");
 });
